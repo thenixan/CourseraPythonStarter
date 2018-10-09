ac79cba3dc455d3f4f96ec1c866f1ecad9a89ce4
Ilya Nixan <thenixan@gmail.com>
2018-10-09 22:40:02 +0300

    Task 13

---
+++ src/task13.py
@@ -0,0 +1,6 @@
+number = int(input())
+
+print('The next number for the number ', number, ' is ', number + 1, '.',
+      sep='')
+print('The previous number for the number ', number, ' is ', number - 1, '.',
+      sep='')
